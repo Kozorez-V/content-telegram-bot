a580267e8d3cadc24bcf280265caf324ec73cbdc
Kozorez-V <kozorezviktoria@gmail.com>
2024-02-25 12:54:41 +0400

    install async sqlaclhemy 2.0

--- bot/services/tags.py
+++ bot/services/tags.py
@@ -1,9 +1,10 @@
-"""Парсер тегов"""
+"""Парсинг и преобразование тегов"""
 
 import re
 from typing import Optional, List
 
 async def parse_tags(message_text: str) -> Optional[List[str]]:
+    """Ищем теги в сообщении и возвращаем список"""
     tag_pattern = r'#\w+'
     tags_list = re.findall(tag_pattern, message_text)
 
