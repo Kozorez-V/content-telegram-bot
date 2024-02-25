3770abc3c0704994a611d59b0241bd8330688fd7
Kozorez-V <kozorezviktoria@gmail.com>
2024-02-25 12:07:04 +0400

    tag parsing

--- bot/client.py
+++ bot/client.py
@@ -4,14 +4,21 @@ import config
 import logging
 from telethon.sync import TelegramClient
 from telethon.tl.types import InputMessagesFilterEmpty
+from services import tags
 
 
 client = TelegramClient('client', config.api_id, config.api_hash).start()
 
 
 async def get_messages(channel):
-    """Парсим все текстовые посты из канала"""
+    """Парсим все текстовые посты из канала и добавляем в БД"""
     
     async for message in client.iter_messages(channel, reverse=True, filter=InputMessagesFilterEmpty):
         if message.text:
-            print(message.id, message.date, message.views, message.text)
+            message_tags = await tags.parse_tags(message.text)
+            if message_tags:
+                print(message_tags)
+            # print(message.id, message.date, message.views)
+            
+        # if message.replies:
+        #     print(message.replies.replies)
---
+++ bot/services/tags.py
@@ -0,0 +1,13 @@
+"""Парсер тегов"""
+
+import re
+from typing import Optional, List
+
+async def parse_tags(message_text: str) -> Optional[List[str]]:
+    tag_pattern = r'#\w+'
+    tags_list = re.findall(tag_pattern, message_text)
+
+    if not tags_list:
+        return None
+    
+    return tags_list
