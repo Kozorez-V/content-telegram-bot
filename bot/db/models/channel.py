256d663b8d15c5a8c14d2d409b40c91863e26dd7
Kozorez-V <kozorezviktoria@gmail.com>
2024-02-27 15:46:45 +0400

    one-to-many between Channel and Post

--- bot/db/models/channel.py
+++ bot/db/models/channel.py
@@ -1,6 +1,7 @@
 from .base import Base
 
 from typing import List
+from .post import Post
 
 from sqlalchemy.orm import (
     Mapped,
@@ -13,3 +14,4 @@ from sqlalchemy.types import BigInteger
 class Channel(Base):
     channel_id: Mapped[BigInteger] = mapped_column(nullable=False)
     username: Mapped[str] = mapped_column(nullable=True)
+    posts: Mapped[List['Post']] = relationship(back_populate='channel')
--- bot/db/models/post.py
+++ bot/db/models/post.py
@@ -7,8 +7,12 @@ from sqlalchemy.orm import (
     mapped_column,
     relationship)
 
+from sqlalchemy import ForeignKey
+
 
 class Post(Base):
     date: Mapped[datetime.datetime] = mapped_column(nullable=False)
     views: Mapped[int] = mapped_column(nullable=True)
     replies: Mapped[int] = mapped_column(nullable=True)
+    channel_id = Mapped[int] = mapped_column(ForeignKey('channels.id'))
+    channel: Mapped['Channel']= relationship(back_populates="posts")
